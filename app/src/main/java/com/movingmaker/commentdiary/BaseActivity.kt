3f5fb1ac828e9d4d526c82c059d09e670cac83c9
dudwls901 <dudwls901@naver.com>
2022-02-28 22:53:24 +0900

    SuppressLint추가

--- app/src/main/java/com/movingmaker/commentdiary/BaseActivity.kt
+++ app/src/main/java/com/movingmaker/commentdiary/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.movingmaker.commentdiary
 
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,7 @@ abstract class BaseActivity<T: ViewDataBinding>: AppCompatActivity() {
     abstract val TAG :String // 액티비티 태그
     abstract val layoutRes: Int // 바인딩에 필요한 layout
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(TAG,"onCreate")
