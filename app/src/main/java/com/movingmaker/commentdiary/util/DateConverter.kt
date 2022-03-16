b9d18ff7dba44437a100e3e39950fb238ed917a3
yeongjin <dudwls901@naver.com>
2022-03-17 02:53:27 +0900

    [Feature] YmFormat 추가

--- app/src/main/java/com/movingmaker/commentdiary/util/DateConverter.kt
+++ app/src/main/java/com/movingmaker/commentdiary/util/DateConverter.kt
@@ -9,7 +9,9 @@ object DateConverter {
 
     fun getCodaToday() = LocalDateTime.now().minusHours(7).toLocalDate()
 
-    fun ymdFormat(date: LocalDate) = "${String.format("%02d", date.year)}.${String.format("%02d", date.monthValue)}.${String.format("%02d", date.dayOfMonth)}"
+    fun ymdFormat(date: LocalDate) = "${date.year}.${String.format("%02d", date.monthValue)}.${String.format("%02d", date.dayOfMonth)}"
+
+    fun ymFormat(date: LocalDate) = "${date.year}.${String.format("%02d",date.monthValue)}"
 
     fun ymdToDate(ymd: String): LocalDate{
         val (y,m,d) = ymd.split('.').map{it.toInt()}
