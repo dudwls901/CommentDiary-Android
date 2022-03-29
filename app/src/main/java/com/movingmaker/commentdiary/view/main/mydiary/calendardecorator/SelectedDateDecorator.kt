bb016ee803de2dd8c974a3f9b9072224b5f2e984
yeongjin <dudwls901@naver.com>
2022-03-30 01:43:25 +0900

    [Style] getDrawable 수정

--- app/src/main/java/com/movingmaker/commentdiary/view/main/mydiary/calendardecorator/SelectedDateDecorator.kt
+++ app/src/main/java/com/movingmaker/commentdiary/view/main/mydiary/calendardecorator/SelectedDateDecorator.kt
@@ -1,10 +1,12 @@
 package com.movingmaker.commentdiary.view.main.mydiary.calendardecorator
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.graphics.Typeface
 import android.graphics.drawable.Drawable
 import android.text.style.StyleSpan
+import androidx.core.content.ContextCompat
 import com.movingmaker.commentdiary.R
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
@@ -12,14 +14,16 @@ import com.prolificinteractive.materialcalendarview.DayViewFacade
 
 //사이즈, 글자 색 반전 등의 커스텀 필요 없으면 xml에서 처리
 class SelectedDateDecorator(context: Context): DayViewDecorator {
-    //drawable 가져오기 패캠에서 찾자
-    private val drawable: Drawable = context.resources.getDrawable(R.drawable.date_selector)
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private val drawable = ContextCompat.getDrawable(context,R.drawable.date_selector)
     override fun shouldDecorate(day: CalendarDay): Boolean {
         return true
     }
 
     override fun decorate(view: DayViewFacade) {
         //R.drawable.date_selector에서 선택한 날짜만 바뀌게 처리
-        view.setSelectionDrawable(drawable)
+        if (drawable != null) {
+            view.setSelectionDrawable(drawable)
+        }
     }
 }
