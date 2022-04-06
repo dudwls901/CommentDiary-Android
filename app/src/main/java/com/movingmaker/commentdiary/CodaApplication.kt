5cd46e67c0cf6898744024213dd2c3761f655725
yeongjin <dudwls901@naver.com>
2022-04-06 22:54:39 +0900

    [Feature] 인앱 업데이트(즉시 업뎃) 코드 추가(테스트 필요)

--- app/src/main/java/com/movingmaker/commentdiary/CodaApplication.kt
+++ app/src/main/java/com/movingmaker/commentdiary/CodaApplication.kt
@@ -4,21 +4,26 @@ import android.app.Application
 import android.content.Intent
 import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.firebase.messaging.FirebaseMessaging
 import com.movingmaker.commentdiary.global.MyFirebaseMessagingService
 import com.movingmaker.commentdiary.model.AuthProvider
 import com.movingmaker.commentdiary.view.onboarding.OnboardingLoginActivity
 
-class CodaApplication(): Application() {
+class CodaApplication : Application() {
 
-    private lateinit var dataStore : AuthProvider
+    private lateinit var dataStore: AuthProvider
 
-//    val database by lazy { CommentDiaryDatabase.getInstance(this) }
+    //    val database by lazy { CommentDiaryDatabase.getInstance(this) }
 //    val repository by lazy { ContactRepository(database!!.contactDao()) }
     companion object {
         private lateinit var codaApplication: CodaApplication
-        fun getInstance() : CodaApplication = codaApplication
-        const val termsUrl =  "https://glittery-silk-987.notion.site/fb0c6c765a7a411c9362dc8d102c95e0"
+        fun getInstance(): CodaApplication = codaApplication
+        const val termsUrl =
+            "https://glittery-silk-987.notion.site/fb0c6c765a7a411c9362dc8d102c95e0"
         const val policyUrl = "https://www.notion.so/59a704f6702f4382b9398fa3b4a0d780"
         lateinit var deviceToken: String
     }
@@ -29,14 +34,15 @@ class CodaApplication(): Application() {
         codaApplication = this
         dataStore = AuthProvider(this)
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task->
-            if(task.isSuccessful){
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
                 deviceToken = task.result
             }
         }
+
     }
 
-    fun logOut(){
+    fun logOut() {
         getInstance().getDataStore().insertAuth("", "", 0L)
         startActivity(Intent(this, OnboardingLoginActivity::class.java).apply {
             //메인 액티비티 실행하면 현재 화면 필요 없으니 cleartask
@@ -45,5 +51,5 @@ class CodaApplication(): Application() {
         })
     }
 
-    fun getDataStore() : AuthProvider = dataStore
+    fun getDataStore(): AuthProvider = dataStore
 }
--- app/src/main/java/com/movingmaker/commentdiary/view/onboarding/SplashActivity.kt
+++ app/src/main/java/com/movingmaker/commentdiary/view/onboarding/SplashActivity.kt
@@ -1,14 +1,20 @@
 package com.movingmaker.commentdiary.view.onboarding
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.movingmaker.commentdiary.CodaApplication
 import com.movingmaker.commentdiary.databinding.ActivitySplashBinding
+import com.movingmaker.commentdiary.global.CodaSnackBar
 import com.movingmaker.commentdiary.util.DateConverter
 import com.movingmaker.commentdiary.view.main.MainActivity
 import com.movingmaker.commentdiary.viewmodel.onboarding.IntroViewModel
@@ -16,7 +22,7 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.first
 import kotlin.coroutines.CoroutineContext
 
-class SplashActivity: AppCompatActivity(),CoroutineScope {
+class SplashActivity : AppCompatActivity(), CoroutineScope {
 
     private lateinit var binding: ActivitySplashBinding
 
@@ -26,14 +32,20 @@ class SplashActivity: AppCompatActivity(),CoroutineScope {
     private val introViewModel: IntroViewModel by viewModels()
     private var pushDate: String? = null
 
+    companion object{
+        const val REQUEST_CODE_UPDATE = 999
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
-        if (intent?.extras != null){
-            for (key: String in intent!!.extras!!.keySet()){
+        updateVersion()
+
+        if (intent?.extras != null) {
+            for (key: String in intent!!.extras!!.keySet()) {
                 val yesterDay = DateConverter.getCodaToday().minusDays(1)
                 pushDate = DateConverter.ymdFormat(yesterDay)
 //                val value = intent!!.extras!!.get(key)
@@ -54,7 +66,7 @@ class SplashActivity: AppCompatActivity(),CoroutineScope {
                 //refresh토큰 갱신하는 api 필요 refreshToken만료됐는지 검사 후 accessToken,refreshToken,expiresIn발급
 //                finish()
                 val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                intent.putExtra("pushDate",pushDate)
+                intent.putExtra("pushDate", pushDate)
 
                 startActivity(intent.apply {
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -62,11 +74,52 @@ class SplashActivity: AppCompatActivity(),CoroutineScope {
                 })
 
             } else {
-                startActivity(Intent(this@SplashActivity, OnboardingIntroActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                })
+                startActivity(
+                    Intent(
+                        this@SplashActivity,
+                        OnboardingIntroActivity::class.java
+                    ).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    })
             }
         }
     }
+
+    //업데이트 확인 + 수락하면 업데이트
+    private fun updateVersion() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+
+// Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+// Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                // This example applies an immediate update. To apply a flexible update
+                // instead, pass in AppUpdateType.FLEXIBLEΩ
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                Log.d("updateeeeee", " in if updateVersion: ${appUpdateInfo}")
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this@SplashActivity,
+                    0
+                )
+            }
+        }
+    }
+
+    //업데이트 취소시
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("updateeeeeee", "onActivityResult: $requestCode $resultCode $data ")
+        if(requestCode== REQUEST_CODE_UPDATE){
+            if(resultCode != Activity.RESULT_OK){
+                CodaSnackBar.make(binding.root, "업데이트가 취소 되었습니다.").show()
+            }
+        }
+    }
+
 }
