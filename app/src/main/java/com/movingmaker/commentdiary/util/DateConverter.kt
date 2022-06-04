2eb772931ad9458aca1b3b5eef1b6544c010657f
yeongjin <dudwls901@naver.com>
2022-06-04 20:31:18 +0900

    [Refactor] +JvmStatic

--- app/src/main/java/com/movingmaker/commentdiary/util/DateConverter.kt
+++ app/src/main/java/com/movingmaker/commentdiary/util/DateConverter.kt
@@ -7,12 +7,16 @@ import java.util.*
 
 object DateConverter {
 
+    @JvmStatic
     fun getCodaToday() = LocalDateTime.now().minusHours(7).toLocalDate()
 
+    @JvmStatic
     fun ymdFormat(date: LocalDate) = "${date.year}.${String.format("%02d", date.monthValue)}.${String.format("%02d", date.dayOfMonth)}"
 
+    @JvmStatic
     fun ymFormat(date: LocalDate) = "${date.year}.${String.format("%02d",date.monthValue)}"
 
+    @JvmStatic
     fun ymdToDate(ymd: String): LocalDate{
         val (y,m,d) = ymd.split('.').map{it.toInt()}
         return LocalDate.of(y,m,d)
--- app/src/main/java/com/movingmaker/commentdiary/util/Extension.kt
+++ app/src/main/java/com/movingmaker/commentdiary/util/Extension.kt
@@ -3,6 +3,6 @@ package com.movingmaker.commentdiary.util
 import android.content.res.Resources
 
 object Extension {
-    fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
-    fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
+    @JvmStatic fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+    @JvmStatic fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
 }
