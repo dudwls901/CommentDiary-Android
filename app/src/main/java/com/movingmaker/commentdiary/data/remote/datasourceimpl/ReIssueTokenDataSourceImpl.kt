43d36c9581ac7dc4d7adb50834c881856c790115
yeongjin <dudwls901@naver.com>
2022-11-06 20:23:15 +0900

    [Refactor] ReIssueApi safeApiCall 적용

--- app/src/main/java/com/movingmaker/commentdiary/data/remote/datasourceimpl/ReIssueTokenDataSourceImpl.kt
+++ app/src/main/java/com/movingmaker/commentdiary/data/remote/datasourceimpl/ReIssueTokenDataSourceImpl.kt
@@ -1,16 +1,18 @@
 package com.movingmaker.commentdiary.data.remote.datasourceimpl
 
+import com.movingmaker.commentdiary.data.model.AuthTokens
 import com.movingmaker.commentdiary.data.remote.api.ReIssueTokenApiService
 import com.movingmaker.commentdiary.data.remote.datasource.ReIssueTokenDataSource
-import com.movingmaker.commentdiary.data.remote.response.LogInResponse
-import retrofit2.Response
+import com.movingmaker.commentdiary.data.util.safeApiCall
+import com.movingmaker.commentdiary.domain.model.BaseResponse
+import com.movingmaker.commentdiary.domain.model.NetworkResult
 import javax.inject.Inject
 
 class ReIssueTokenDataSourceImpl @Inject constructor(
     private val reIssueTokenApiService: ReIssueTokenApiService
-): ReIssueTokenDataSource{
+) : ReIssueTokenDataSource {
 
-    override suspend fun reIssueToken(): Response<LogInResponse> {
-        return reIssueTokenApiService.reIssueToken()
-    }
+    override suspend fun reIssueToken(): NetworkResult<BaseResponse<AuthTokens>> =
+        safeApiCall { reIssueTokenApiService.reIssueToken() }
+
 }
