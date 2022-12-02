0c2d094753340258f0df3ed2101fe7b1c75cda19
yeongjin <dudwls901@naver.com>
2022-12-02 16:34:13 +0900

    [Chore] buildSrc 생성 (kts)

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,72 @@
+object AndroidX {
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
+}
+
+object Ktx {
+    const val CORE = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+    const val ACTIVITY = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX}"
+    const val FRAGMENT = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_KTX}"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+}
+
+object Test {
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+}
+
+object AndroidTest {
+    const val EXT_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE}"
+}
+
+object Hilt {
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+    const val HILT_CORE = "com.google.dagger:hilt-core:${Versions.HILT}"
+}
+
+object Retrofit {
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val CONVERTER_SERIALIZATION =
+        "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:${Versions.RETROFIT_SERIALIZATION_CONVERTER}"
+}
+
+object OkHttp {
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+    const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+}
+
+object Room {
+    const val ROOM_RUNTIME = "androidx.room:room-runtime:${Versions.ROOM}"
+    const val ROOM_KTX = "androidx.room:room-ktx:${Versions.ROOM}"
+    const val ROOM_COMPILER = "androidx.room:room-compiler:${Versions.ROOM}"
+}
+
+object Coroutines {
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINE}"
+}
+
+object Lifecycle {
+    const val LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.LIFECYCLE}"
+    const val LIFECYCLE_VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.LIFECYCLE}"
+    const val LIFECYCLE_LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIFECYCLE}"
+}
+
+object Timber {
+    const val TIMBER = "com.jakewharton.timber:timber:${Versions.TIMBER}"
+}
+
+object Serialization {
+    const val SERIALIZATION = "org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.SERIALIZATION}"
+}
+
+object NaverMap {
+    const val NAVER_MAP = "com.naver.maps:map-sdk:${Versions.NAVER_MAP}"
+}
+
+object GooglePlay {
+    const val location = "com.google.android.gms:play-services-location:${Versions.GMS_LOCATION}"
+}
