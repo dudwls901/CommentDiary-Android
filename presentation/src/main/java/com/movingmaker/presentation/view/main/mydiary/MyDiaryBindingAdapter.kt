4e8d54e5df2301b7cd9efec76168a20a0f5c8611
yeongjin <dudwls901@naver.com>
2022-12-18 15:53:30 +0900

    [UI] Write Comment Diary Hint 변경

---
+++ presentation/src/main/java/com/movingmaker/presentation/view/main/mydiary/MyDiaryBindingAdapter.kt
@@ -0,0 +1,40 @@
+package com.movingmaker.presentation.view.main.mydiary
+
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatButton
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
+import com.movingmaker.presentation.R
+import com.movingmaker.presentation.util.DIARY_TYPE
+
+
+@BindingAdapter("app:diaryHint")
+fun EditText.bindDiaryHint(diaryType: LiveData<DIARY_TYPE>) {
+    hint = when (diaryType.value) {
+        DIARY_TYPE.COMMENT_DIARY -> context.getString(R.string.write_comment_diary_content_hint)
+        else -> context.getString(R.string.write_alone_diary_content_hint)
+    }
+}
+
+@BindingAdapter("app:colorWithDiaryType")
+fun TextView.bindColorWithDiaryType(diaryType: LiveData<DIARY_TYPE>) {
+    setTextColor(
+        when (diaryType.value) {
+            DIARY_TYPE.COMMENT_DIARY -> context.getColor(R.color.core_green)
+            else -> context.getColor(R.color.core_orange)
+        }
+    )
+}
+
+@BindingAdapter("app:sendButtonState")
+fun AppCompatButton.bindSendButtonState(canSendCommentDiary: LiveData<Boolean>) {
+    background = when (canSendCommentDiary.value) {
+        true -> AppCompatResources.getDrawable(context, R.drawable.background_green_radius_10)
+        else -> AppCompatResources.getDrawable(
+            context,
+            R.drawable.background_green_alpha_40_radius_10
+        )
+    }
+}
