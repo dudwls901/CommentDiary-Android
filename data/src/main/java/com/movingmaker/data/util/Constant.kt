74e57fcf0aa4526e231dc3fcc2be844b33804faf
yeongjin <dudwls901@naver.com>
2023-03-01 22:56:08 +0900

    [Update] 서버 도메인 변경

--- data/src/main/java/com/movingmaker/data/util/Constant.kt
+++ data/src/main/java/com/movingmaker/data/util/Constant.kt
@@ -3,7 +3,7 @@ package com.movingmaker.data.util
 
 // https://comment-diary.shop/api/v1/ 운영 서버
 // http://jwyang.shop:8080/api/v1/ 테스트 서버
-const val BASE_URL = "https://comment-diary.shop/api/v1/"
+const val BASE_URL = "https://comment-diary.store/api/v1/"
 
 //    const val BASE_URL = "http://jwyang.shop:8080/api/v1/"
 const val SEND_EMAIL_CODE = "email"
