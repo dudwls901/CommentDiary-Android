85fb39ed76f94f8512fe5d4dd4b40cc44bb9ed41
dudwls901 <dudwls901@naver.com>
2023-07-30 17:26:42 +0900

    [Fix] ErrorResponse status 필드 없는 경우 대응

--- data/src/main/java/com/movingmaker/data/remote/model/response/ErrorResponse.kt
+++ data/src/main/java/com/movingmaker/data/remote/model/response/ErrorResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ErrorResponse(
-    val status: Int,
+    val status: Int? = null,
     val code: String,
     val message: String
 )
