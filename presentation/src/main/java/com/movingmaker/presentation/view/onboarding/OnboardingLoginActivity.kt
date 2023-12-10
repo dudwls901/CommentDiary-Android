e6fe296477823e3eb19ca8f73a3f314bb3153ae5
dudwls901 <dudwls901@naver.com>
2023-12-10 20:39:12 +0900

    온보딩 화면 자체 로그인, 카카오 로그인 제거

--- presentation/src/main/java/com/movingmaker/presentation/view/onboarding/OnboardingLoginActivity.kt
+++ presentation/src/main/java/com/movingmaker/presentation/view/onboarding/OnboardingLoginActivity.kt
@@ -1,6 +1,5 @@
 package com.movingmaker.presentation.view.onboarding
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.viewModels
@@ -9,8 +8,6 @@ import androidx.navigation.fragment.NavHostFragment
 import com.movingmaker.presentation.R
 import com.movingmaker.presentation.base.BaseActivity
 import com.movingmaker.presentation.databinding.ActivityOnboardingLoginBinding
-import com.movingmaker.presentation.util.FRAGMENT_NAME
-import com.movingmaker.presentation.util.event.EventObserver
 import com.movingmaker.presentation.view.snackbar.CodaSnackBar
 import com.movingmaker.presentation.viewmodel.onboarding.OnboardingViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +20,6 @@ class OnboardingLoginActivity :
     private var backButtonTime = 0L
     private lateinit var navController: NavController
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
@@ -34,62 +30,13 @@ class OnboardingLoginActivity :
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.onboardingNavHostFragment) as NavHostFragment
         navController = navHostFragment.navController
-//        initViews()
-//        observeDatas()
     }
-    private fun observeDatas() {
-
-        onboardingViewModel.snackMessage.observe(this, EventObserver {
-            CodaSnackBar.make(binding.root, it).show()
-        })
-
-        onboardingViewModel.currentFragment.observe(this) { fragment ->
-            when (fragment) {
-
-                FRAGMENT_NAME.LOGIN -> {
-                    //로그인 화면으로 되돌아올 때 email, password 초기화
-                    onboardingViewModel.clearEmail()
-                    onboardingViewModel.clearPassword()
-                }
-
-//                FRAGMENT_NAME.KAKAO_TERMS -> {
-//                    onboardingViewModel.isPolicyAccept.observe(this) {
-//                        setTermsButtonState(it && onboardingViewModel.isTermsAccept.value!!)
-//                    }
-//                    onboardingViewModel.isTermsAccept.observe(this) {
-//                        setTermsButtonState(it && onboardingViewModel.isTermsAccept.value!!)
-//                    }
-//                    binding.onboardingBottomButton.setOnSingleClickListener {
-//                        kakaoSignUp()
-//                    }
-//                }
-
-                else -> {}
-            }
-        }
-    }
-
-//    private fun kakaoSignUp() {
-//        lifecycleScope.launch {
-//            if (onboardingViewModel.kakaoSignUpSetAccepts()) {
-//                startActivity(
-//                    Intent(
-//                        this@OnboardingLoginActivity,
-//                        MainActivity::class.java
-//                    ).apply {
-//                        finish()
-//                    })
-//            }
-//        }
-//    }
-
 
     override fun onBackPressed() {
         val currentTime = System.currentTimeMillis()
         val gapTime = currentTime - backButtonTime
 
-        if (navController.currentDestination!!.id == R.id.onboardingLoginFragment
-        ) {
+        if (navController.currentDestination!!.id == R.id.onboardingLoginFragment) {
             if (gapTime in 0..2000) {
                 // 2초 안에 두번 뒤로가기 누를 시 앱 종료
                 finish()
--- presentation/src/main/java/com/movingmaker/presentation/viewmodel/onboarding/OnboardingViewModel.kt
+++ presentation/src/main/java/com/movingmaker/presentation/viewmodel/onboarding/OnboardingViewModel.kt
@@ -2,30 +2,16 @@ package com.movingmaker.presentation.viewmodel.onboarding
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import com.movingmaker.domain.model.UiState
-import com.movingmaker.domain.usecase.ClearCachedDiariesUseCase
-import com.movingmaker.domain.usecase.SignOutUseCase
 import com.movingmaker.presentation.base.BaseViewModel
-import com.movingmaker.presentation.util.EMPTY_USER
 import com.movingmaker.presentation.util.FRAGMENT_NAME
-import com.movingmaker.presentation.util.PreferencesUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
-import javax.inject.Inject
 
 @HiltViewModel
-class OnboardingViewModel @Inject constructor(
-    private val preferencesUtil: PreferencesUtil,
-    private val signOutUseCase: SignOutUseCase,
-    private val clearCachedDiariesUseCase: ClearCachedDiariesUseCase
-) : BaseViewModel() {
+class OnboardingViewModel : BaseViewModel() {
 
     private var _currentFragment = MutableLiveData<FRAGMENT_NAME>().apply {
         FRAGMENT_NAME.SIGNUP_TERMS
     }
-    val currentFragment: LiveData<FRAGMENT_NAME>
-        get() = _currentFragment
 
     //for signUp, logIn data share
     private var _isTermsAccept = MutableLiveData<Boolean>().apply { value = false }
@@ -43,11 +29,6 @@ class OnboardingViewModel @Inject constructor(
     private var _isAllAccept = MutableLiveData<Boolean>().apply { value = false }
     val isAllAccept: LiveData<Boolean>
         get() = _isAllAccept
-
-    var email = MutableLiveData<String>().apply { value = "" }
-    var password = MutableLiveData<String>().apply { value = "" }
-
-    var code = MutableLiveData<String>()
 
     fun setCurrentFragment(fragment: FRAGMENT_NAME) {
         _currentFragment.value = fragment
@@ -87,98 +68,5 @@ class OnboardingViewModel @Inject constructor(
         }
     }
 
-    fun clearEmail() {
-        email.value = ""
-    }
-
-    fun clearPassword() {
-        password.value = ""
-    }
-
-//    suspend fun kakaoLogin(kakaoAccessToken: String) = viewModelScope.async {
-//        var successLogin = false
-//        var isNewMember = false
-//        with(
-//            kakaoLogInUseCase(
-//                KakaoLoginModel(
-//                    KAKAO, kakaoAccessToken, preferencesUtil.getDeviceToken()
-//                )
-//            )
-//        ) {
-//            when (this) {
-//                is UiState.Success -> {
-//                    successLogin = true
-//                    val accessToken = this.data.authTokens.accessToken
-//                    val refreshToken = this.data.authTokens.refreshToken
-//                    val accessTokenExpiresIn = this.data.authTokens.accessTokenExpiresIn
-//                    val userId = this.data.userId
-//                    isNewMember = this.data.authTokens.isNewMember == true
-//                    Timber.d(
-//                        "kakaoLogin: ${accessToken} ${refreshToken} ${accessTokenExpiresIn}"
-//                    )
-//                    clearCachedDiaries(userId)
-//                    preferencesUtil
-//                        .insertAuth(KAKAO, accessToken, refreshToken, accessTokenExpiresIn, userId)
-//                }
-//                is UiState.Error -> {
-//                    setMessage(message)
-//                }
-//                is UiState.Fail -> {
-//                    setMessage(message)
-//                }
-//            }
-//        }
-//        Pair(successLogin, isNewMember)
-//    }.await()
-
-    private suspend fun clearCachedDiaries(userId: Long) {
-        val beforeUserId = preferencesUtil.getUserId()
-        if (beforeUserId != EMPTY_USER && beforeUserId != userId) {
-            clearCachedDiariesUseCase()
-        }
-    }
-
-//    suspend fun kakaoSignUpSetAccepts() = viewModelScope.async {
-//        onLoading()
-//        var successSignUp = false
-//        val kakaoSignUpRequest =
-//            KakaoSignUpModel(if (isPushAccept.value == true) 'Y' else 'N')
-//        with(kakaoSignUpSetAcceptsUseCase(kakaoSignUpRequest)) {
-//            offLoading()
-//            Timber.d("result $this")
-//            when (this) {
-//                is UiState.Success -> {
-//                    successSignUp = true
-//                }
-//                is UiState.Error -> {
-//                    setMessage(message)
-//                }
-//                is UiState.Fail -> {
-//                    setMessage(message)
-//                }
-//            }
-//        }
-//        successSignUp
-//    }.await()
-
-    suspend fun signOut() = viewModelScope.async {
-        onLoading()
-        var successSignOut = false
-        with(signOutUseCase()) {
-            when (this) {
-                is UiState.Success -> {
-                    successSignOut = true
-                    preferencesUtil.signOut()
-                }
-                is UiState.Error -> {
-                    setMessage(message)
-                }
-                is UiState.Fail -> {
-                    setMessage(message)
-                }
-            }
-        }
-        successSignOut
-    }.await()
-
+    private suspend fun clearCachedDiaries(userId: Long) {}
 }
