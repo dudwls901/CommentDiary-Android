29cd218f1d053a70715d5bad4ea824140394acac
dudwls901 <dudwls901@naver.com>
2024-02-04 23:02:54 +0900

    임시 로그아웃 구현

--- presentation/src/main/java/com/movingmaker/presentation/view/main/mypage/MyAccountFragment.kt
+++ presentation/src/main/java/com/movingmaker/presentation/view/main/mypage/MyAccountFragment.kt
@@ -1,16 +1,23 @@
 package com.movingmaker.presentation.view.main.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.movingmaker.presentation.R
 import com.movingmaker.presentation.base.BaseFragment
 import com.movingmaker.presentation.databinding.FragmentMypageMyaccountBinding
 import com.movingmaker.presentation.util.FRAGMENT_NAME
+import com.movingmaker.presentation.view.onboarding.OnboardingLoginActivity
 import com.movingmaker.presentation.viewmodel.FragmentViewModel
+import com.movingmaker.presentation.viewmodel.mypage.MyPageScreenUiEvent
 import com.movingmaker.presentation.viewmodel.mypage.MyPageViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class MyAccountFragment :
@@ -24,12 +31,10 @@ class MyAccountFragment :
         fragmentViewModel.setCurrentFragment(FRAGMENT_NAME.MY_ACCOUNT)
         binding.vm = myPageViewModel
         initViews()
+        observeDatas()
     }
 
     private fun initViews() = with(binding) {
-        logoutLayout.setOnClickListener {
-            myPageViewModel.logout()
-        }
         signOutLayout.setOnClickListener {
             val action = MyAccountFragmentDirections.actionMyAccountFragmentToSignOutFragment()
             findNavController().navigate(action)
@@ -44,5 +49,23 @@ class MyAccountFragment :
         }
     }
 
+    private fun observeDatas() {
+        myPageViewModel.uiEvent
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach { handleEvent(it) }
+            .launchIn(lifecycleScope)
+    }
+
+    private fun handleEvent(event: MyPageScreenUiEvent) {
+        when (event) {
+            MyPageScreenUiEvent.Logout -> {
+                startActivity(
+                    Intent(requireContext(), OnboardingLoginActivity::class.java).apply {
+                        requireActivity().finish()
+                    }
+                )
+            }
+        }
+    }
 
 }
